fd91c5a49b8285f52522f96f5a937342ff64ab4a
Szczepan <cyber@gawliknet.eu>
2026-03-14 20:06:36 +0100

    fix: register classes by fabric handlers

--- src/main/clojure/tennisball/client.clj
+++ src/main/clojure/tennisball/client.clj
@@ -1,18 +1,6 @@
-(ns tennisball.client
-  (:import [com.tennisball TennisBallBridge]
-           [net.fabricmc.fabric.api.client.rendering.v1 EntityRendererRegistry]
-           [net.minecraft.client.render.entity FlyingItemEntityRenderer]))
-
-(defn- register-client-renderer! []
-  (when-let [type (TennisBallBridge/entityType)]
-    (EntityRendererRegistry/register
-     type
-     (reify net.minecraft.client.render.entity.EntityRendererFactory
-       (create [_ ctx]
-         (FlyingItemEntityRenderer. ctx))))))
+(ns tennisball.client)
 
 ;; Client-side initialization
 (defn init-client []
   (println "Tennis Ball Mod - Client initializing...")
-  (register-client-renderer!)
   (println "Tennis Ball Mod - Client initialized!"))
